3de40381da8b9771691fa4093006917ac07e8101
Matt Langeman <matt@langeman.net>
2020-08-21 14:05:47 -0400

    add migration to run link_ms_accounts and setup file for pip installing

---
+++ __init__.py
@@ -0,0 +1,4 @@
+__title__ = 'Django MSAL'
+__version__ = '0.1.0'
+__author__ = 'Matt Langeman'
+__email__ = 'Matthew_Langeman@dai.com'
---
+++ setup.py
@@ -0,0 +1,37 @@
+import re
+import setuptools
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+def get_version():
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open('__init__.py').read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+version = get_version()
+
+setuptools.setup(
+    name='django_msal',
+    version=version,
+    author='Matt Langeman',
+    author_email='Matthew_Langeman@dai.com',
+    description='A small Django app that enables SSO with Microsoft Azure Active Directory',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dai-ictgeo/django_msal',
+    keywords='django auth msal microsoft azure',
+    install_requires=['Django >= 2.2',
+                      'msal >= 1.4.3'
+                    ],
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+)
